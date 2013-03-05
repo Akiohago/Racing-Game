02f0a459a2f5624f2ccdfeefaf0ce8867aeb0176
fern <n8fern@gmail.com>
2013-03-05 10:06:56 -0800

    adding scenes to build settings

--- RacingGame/Assets/UI/HealthBar.cs
+++ RacingGame/Assets/UI/HealthBar.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using System.Collections;
 
 
-public class NewBehaviourScript : MonoBehaviour{ 
+public class HealthBar: MonoBehaviour{ 
 	public Texture2D test;
 	void OnGUI(){
 	GUI.Box(new Rect(0,415,250,100),(test));
