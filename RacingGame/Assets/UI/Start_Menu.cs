862521d9173b21c43f8a8ebfbf223ff5f6ddbd71
Stuart Dubin <Dubin.Stuart@gmail.com>
2013-03-12 16:00:41 -0700

    Updated to the Update

--- RacingGame/Assets/UI/Start_Menu.cs
+++ RacingGame/Assets/UI/Start_Menu.cs
@@ -5,11 +5,8 @@ using System.Collections;
 public class Start_Menu : MonoBehaviour{ 
 	public Texture2D menu;
 	void OnGUI(){
-		GUI.Box(new Rect(0,0,950,475),(menu));
-		ToCarSelect();
-	}
-	void ToCarSelect(){
-		if (GUI.Button(new Rect(375,55,200,50)," "))
-		Application.LoadLevel("CarSelector");
+	GUI.Box(new Rect(0,0,950,475),(menu));
+	if (GUI.Button(new Rect(375,50,200,50), " "))
+		Application.LoadLevel("CarSelector");	
 	}
 }
